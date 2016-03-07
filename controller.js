08a4f892a819ef501d2ed1b40878df274777f0f2
Zack Mance <zmance@gmail.com>
2016-03-07 14:21:14 -0500

    adds some code

--- controller.js
+++ controller.js
@@ -1,10 +1,15 @@
 var Controller = function(todoList, view){
   this.view = view;
   this.todoList = todoList;
-}
+};
 
 Controller.prototype.createTask = function( title ){
   var task = new Task(title);
-  var htmlTask = this.view.drawTask(task);
-  this.view.addTaskToList(htmlTask);
-}
+  this.todoList.addTask(task);
+  this.view.clearList();
+  this.view.drawList(this.todoList);
+};
+
+Controller.prototype.ShowList = function(){
+  this.view.drawList(this.todoList);
+};
--- view.js
+++ view.js
@@ -1,16 +1,36 @@
 var View = function(){
   this.addTaskForm = document.getElementById('addTask');
-  this.taskListDiv = document.getElementById('list');
+  this.taskList = document.getElementById('list');
   this.controller = undefined; 
+  this.init();
 };
 
+View.prototype.init = function(){
+  this.addTaskForm.addEventListener('submit', function( event ){
+    event.preventDefault();
+    input = event.target.querySelector('[name="taskName"]').value
+    this.controller.createTask(input);
+  }.bind(this));
+};
 
 View.prototype.addTaskToList = function( htmlTask ){
-  this.taskListDiv.appendChild( htmlTask ) 
-}
+  this.taskList.appendChild( htmlTask ) 
+};
 
-View.prototype.drawTask = function(task){
+View.prototype.drawTask = function( task ){
   var taskElm = document.createElement('li');
-  taskElm.innerHTML = (task.completed ? "[X]" : "[ ]") + ":" + task.title;
-  return taskElm;
-}
+  taskElm.innerHTML = (task.completed ? "[X]" : "[ ]") + ": " + task.title;
+  this.addTaskToList( taskElm );
+};
+
+View.prototype.clearList = function(){
+  while( this.taskList.firstChild ){
+    this.taskList.removeChild( this.taskList.firstChild );
+  }
+};
+
+View.prototype.drawList = function( list ){
+  list.tasks.forEach(function( item ){
+    this.drawTask(item);
+  }.bind(this));
+};
